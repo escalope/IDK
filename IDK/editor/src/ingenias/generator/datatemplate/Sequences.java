2102dfa70c5301519097b7d6abbb971aabf0b980
escalope <jjgomez@fdi.ucm.es>
2008-08-22 05:46:11 +0000

    git-svn-id: svn://svn.code.sf.net/p/ingenias/code/trunk@95 95f194ad-9441-0410-9784-e312ea4ffdb5

--- IDK/editor/src/ingenias/generator/datatemplate/Sequences.java
+++ IDK/editor/src/ingenias/generator/datatemplate/Sequences.java
@@ -19,32 +19,77 @@
     along with INGENIAS IDE; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-*/
+ */
 
 package ingenias.generator.datatemplate;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.CharBuffer;
 import java.util.*;
+
+import org.apache.xerces.parsers.DOMParser;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 public class Sequences {
 
-  private Vector body=new Vector();
+	private Vector body=new Vector();
 
-  public void addRepeat(Repeat r){
-    body.add(r);
-  }
+	public void addRepeat(Repeat r){
+		body.add(r);
+	}
 
-  public void addVar(Var v){
-    body.add(v);
-  }
+	public void addVar(Var v){
+		body.add(v);
+	}
 
-  public String toString(){
-    String result="<sequences>\n";
-    Enumeration enumeration=body.elements();
-    while (enumeration.hasMoreElements()){
-      result=result+enumeration.nextElement().toString();
-    }
+	public String toString(){
+		String ctype="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n";
+		String result=ctype+"<sequences>\n";
+		Enumeration enumeration=body.elements();
+		while (enumeration.hasMoreElements()){
+			result=result+enumeration.nextElement().toString();
+		}
 
-    result=result+"</sequences>\n";
-    return result;
-  }
+		result=result+"</sequences>\n";
+
+		java.io.ByteArrayOutputStream ba=new java.io.ByteArrayOutputStream();
+		OutputStreamWriter osw;
+		try {
+			osw = new OutputStreamWriter(ba,"ISO-8859-1");
+			osw.write(result);
+			osw.close();
+		} catch (UnsupportedEncodingException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		try {
+			result=new String(ba.toByteArray(),"ISO-8859-1");
+		} catch (UnsupportedEncodingException e) {
+
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static void main(String args[]) throws SAXException, IOException{
+		String cadena="<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<v>qué</v>";
+		CharBuffer result = java.nio.CharBuffer.wrap(cadena.toCharArray());
+		cadena=new String(java.nio.charset.Charset.forName("ISO-8859-1").newEncoder().encode(result).array(), "ISO-8859-1");
+		DOMParser parser = new DOMParser();
+		//  Parse the Document
+		//  and traverse the DOM
+		System.err.println(cadena);
+		//	parser.setIncludeIgnorableWhitespace(false);
+		parser.parse(new org.xml.sax.InputSource(new java.io.
+				StringBufferInputStream(cadena)));
+		Document document = parser.getDocument();
+
+	}
 }

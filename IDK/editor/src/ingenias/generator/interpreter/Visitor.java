8a428e4ff12a9ad76de342a0e5c5ce6b2e685456
escalope <jjgomez@fdi.ucm.es>
2009-05-09 12:04:49 +0000

    git-svn-id: svn://svn.code.sf.net/p/ingenias/code/trunk@128 95f194ad-9441-0410-9784-e312ea4ffdb5

--- IDK/editor/src/ingenias/generator/interpreter/Visitor.java
+++
@@ -1,6 +0,0 @@
-package ingenias.generator.interpreter;
-
-public interface Visitor {
-  public void analyze(Object o);
-  public Object getResult();
-}
--- IDK/editor/src/ingenias/generator/interpreter/VisitorWithTreeAccess.java
+++
@@ -1,6 +0,0 @@
-package ingenias.generator.interpreter;
-
-public interface VisitorWithTreeAccess {
-	 public void analyze(Object o, TemplateTree current);
-     public Object getResult();
-}

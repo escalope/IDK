ab893585a5c72110159ebdfe36d404b5aa298268
escalope <jjgomez@fdi.ucm.es>
2009-05-09 08:28:57 +0000

    git-svn-id: svn://svn.code.sf.net/p/ingenias/code/trunk@116 95f194ad-9441-0410-9784-e312ea4ffdb5

--- IDK/editor/src/ingenias/editor/BusyMessageWindow.java
+++ IDK/editor/src/ingenias/editor/BusyMessageWindow.java
@@ -4,56 +4,25 @@ import java.awt.Frame;
 import java.awt.GraphicsConfiguration;
 import java.awt.Window;
 
+import javax.swing.JFrame;
 import javax.swing.JWindow;
 
 public class BusyMessageWindow extends JWindow {
 	IDEState ids=null;
 
-	public BusyMessageWindow(IDEState ids) {
-		ids.setBusy();
-		this.ids=ids;
-	}
-
-	public BusyMessageWindow(IDEState ids, GraphicsConfiguration gc) {
-		super(gc);
-		ids.setBusy();
-		this.ids=ids;
-		
-	}
-
-	public BusyMessageWindow(IDEState ids, Frame owner) {
+	public BusyMessageWindow(IDEState ids, JFrame owner) {
 		super(owner);
 		ids.setBusy();
 		this.ids=ids;
 	}
 
-	public BusyMessageWindow(IDEState ids, Window owner) {
-		super(owner);
-		ids.setBusy();
-		this.ids=ids;
-	}
-
-	public BusyMessageWindow(IDEState ids, Window owner, GraphicsConfiguration gc) {
-		super(owner, gc);
-		ids.setBusy();
-		this.ids=ids;
-	}
-
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		super.hide();
-		ids.setNotBusy();
-	}
 
 	@Override
 	public void setVisible(boolean b) {
 		// TODO Auto-generated method stub
 		super.setVisible(b);
-		if (b)
-			ids.setBusy();
-		else
-			ids.setNotBusy();
+		if (!b)
+		 ids.setNotBusy();
 	}
 	
 	

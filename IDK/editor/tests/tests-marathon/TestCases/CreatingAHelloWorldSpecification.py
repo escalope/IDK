d32cc00de339a749e7eeb14783f4744f254cdb4f
escalope <jjgomez@fdi.ucm.es>
2009-10-07 16:10:58 +0000

    modified tests
    git-svn-id: svn://svn.code.sf.net/p/ingenias/code/trunk@264 95f194ad-9441-0410-9784-e312ea4ffdb5

---
+++ IDK/editor/tests/tests-marathon/TestCases/CreatingAHelloWorldSpecification.py
@@ -0,0 +1,7 @@
+#{{{ Marathon Fixture
+from default import *
+#}}} Marathon Fixture
+
+def test():
+	java_recorded_version = '1.6.0_13'
+ 
---
+++ IDK/editor/tests/tests-marathon/TestCases/ManagingEntities/copying/duplicateConnectedEntitiesIntoAnotherDiagram.py
@@ -0,0 +1,29 @@
+#{{{ Marathon Fixture
+from default import *
+from createEnvironmentDiagram import *	
+#}}} Marathon Fixture
+def test():
+	java_recorded_version = '1.6.0_13'
+	if window('INGENIAS Development Kit'):
+		generateEmptyEnvironmentDiagram('e1')
+		#Creates two entites and connects them
+		createEnvironmentEntitiesAndConnectThem()
+		#The panel shows two entities connected with a relationship
+		assert_p('e1', 'Component.Roots', '[Agent0, Application0, 0:EPerceives, EPerceivessourceRole0:EPerceivessourceRole, EPerceivestargetRole0:EPerceivestargetRole]')
+		#One single edge of the relationship is selected
+		click('e1', 126, 145)
+		assert_p('e1', 'Component.SelectionCells', '[EPerceivestargetRole0:EPerceivestargetRole]')		
+		#click('e1', 298, 132)
+		#click('e1', 122, 137)		
+		keystroke('e1', 'Ctrl+C')
+		#Target diagram is created
+		generateEmptyEnvironmentDiagram('e2')
+		doubleclick('ProjectsTree', 'e2')
+		#The new set of entities is created into the other diagram		
+		keystroke('e2', 'Ctrl+V')		
+		#Every element is duplicated
+		assert_p('e2', 'Component.Roots', '[Agent0, Application0, 0:EPerceives, EPerceivessourceRole0:EPerceivessourceRole, EPerceivestargetRole0:EPerceivestargetRole]')
+		doubleclick('ProjectsTree', 'e1')
+		assert_p('e1', 'Component.Roots', '[Agent0, Application0, 0:EPerceives, EPerceivessourceRole0:EPerceivessourceRole, EPerceivestargetRole0:EPerceivestargetRole]')
+	close()
+

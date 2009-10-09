96d9fce094ce427caef8def293f4401d760e5b7d
escalope <jjgomez@fdi.ucm.es>
2009-10-09 15:36:05 +0000

    test did not work because entities were not created properly. The recreation of the diagram entities has modified previous coordinates, hence, all raw events had to be rewritten
    git-svn-id: svn://svn.code.sf.net/p/ingenias/code/trunk@273 95f194ad-9441-0410-9784-e312ea4ffdb5

--- IDK/editor/tests/tests-marathon/TestCases/ManagingEntities/copying/duplicateConnectedEntitiesByChoosingPartOfTheNetwork.py
+++ IDK/editor/tests/tests-marathon/TestCases/ManagingEntities/copying/duplicateConnectedEntitiesByChoosingPartOfTheNetwork.py
@@ -3,7 +3,9 @@ from default import *
 from createEnvironmentDiagram import *	
 #}}} Marathon Fixture
 def test():
-	java_recorded_version = '1.6.0_13'
+	java_recorded_version = '1.6.0_12'
+	from createEnvironmentDiagram import createEnvironmentEntitiesAndConnectThem
+	from createEnvironmentDiagram import generateEmptyEnvironmentDiagram
 	if window('INGENIAS Development Kit'):
 		generateEmptyEnvironmentDiagram('e1')
 		#Creates two entites and connects them
@@ -11,19 +13,20 @@ def test():
 		#The panel shows two entities connected with a relationship
 		assert_p('e1', 'Component.Roots', '[Agent0, Application0, 0:EPerceives, EPerceivessourceRole0:EPerceivessourceRole, EPerceivestargetRole0:EPerceivestargetRole]')
 		#One single edge of the relationship is selected
-		click('e1', 126, 145)
-		assert_p('e1', 'Component.SelectionCells', '[EPerceivestargetRole0:EPerceivestargetRole]')		
+		click('e1', 110, 80)
+		assert_p('e1', 'Component.SelectionCells', '[EPerceivessourceRole0:EPerceivessourceRole]')		
 		#click('e1', 298, 132)
 		#click('e1', 122, 137)		
 		keystroke('e1', 'Ctrl+C')
 	    #As a result, the whole relationship is selected		
-		assert_p('e1', 'Component.SelectionCells', '[EPerceivestargetRole0:EPerceivestargetRole, Application0, 0:EPerceives, EPerceivessourceRole0:EPerceivessourceRole, Agent0]')
+		assert_p('e1', 'Component.SelectionCells', '[EPerceivessourceRole0:EPerceivessourceRole, Agent0, 0:EPerceives, EPerceivestargetRole0:EPerceivestargetRole, Application0]')
 		#Now, an edge and one end of the relationship are chosen
-		click('e1', 267, 173)
-		click('e1', 251, 150)
-		click('e1', 118, 140)
-		click('e1', 141, 173, 'Ctrl')
-		assert_p('e1', 'Component.SelectionCells', '[EPerceivestargetRole0:EPerceivestargetRole, Application0]')
+		click('e1', 300, 110) #to remove previous selection
+		click('e1', 90, 110)
+		click('e1', 100, 140,'Ctrl')
+		#click('e1', 118, 140)
+		#click('e1', 60, 70, 'Ctrl')
+		assert_p('e1', 'Component.SelectionCells', '[EPerceivestargetRole0:EPerceivestargetRole, Application0]') #remember to leave a blank after each comma
 		#After selected, we copy the whole by means of the keyboard shortcut
 		keystroke('e1', 'Ctrl+C')
 		keystroke('e1', 'Ctrl+V')
@@ -31,4 +34,5 @@ def test():
 		#assert_p('e1', 'Component.SelectionCells', '[Agent0, EPerceivessourceRole0:EPerceivessourceRole, EPerceivestarget, 0:EPerceives, Agent0, EPerceivestargetRole0:EPerceivestargetRole, EPerceivessource,  Application0, Application0]')
 		#Every element is duplicated
 		assert_p('e1', 'Component.Roots', '[Agent0, Application0, 0:EPerceives, EPerceivessourceRole0:EPerceivessourceRole, EPerceivestargetRole0:EPerceivestargetRole, Agent0, Application0, 0:EPerceives, EPerceivessourceRole0:EPerceivessourceRole, EPerceivestargetRole0:EPerceivestargetRole]')
-	close()
+	close()	
+

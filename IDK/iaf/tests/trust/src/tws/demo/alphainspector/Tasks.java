7d21f70eccea33a3a45dd7ea7490052da2a08dfe
escalope <jjgomez@fdi.ucm.es>
2009-10-26 00:15:32 +0000

    update IAF
    git-svn-id: svn://svn.code.sf.net/p/ingenias/code/trunk@316 95f194ad-9441-0410-9784-e312ea4ffdb5

--- IDK/iaf/tests/trust/src/tws/demo/alphainspector/Tasks.java
+++ IDK/iaf/tests/trust/src/tws/demo/alphainspector/Tasks.java
@@ -5,6 +5,8 @@
 
 package tws.demo.alphainspector;
 
+import java.util.Random;
+
 import ingenias.jade.components.TaskOutput;
 import ingenias.jade.mental.QualityDegreeOfSourceInTesting;
 import ingenias.jade.mental.InspectQualityOfSourceInTesting;
@@ -16,32 +18,88 @@ import tws.demo.AlphaInspection;
  * @author carlos
  */
 public class Tasks {
+	private static Random randomSubject=new Random();
+	private static Random randomObject=new Random();
+	private static Random randomProcess=new Random();
 
-    public static void processAlphaQualityInspectionRequestTaskTask(
-            InspectQualityOfSourceInTesting eiInspeccionarCalidadFuenteCuarentena,
-            QualityDegreeOfSourceInTesting outputsdefaultGradoCalidadFuenteCuarentena,
-            TaskOutput outputsdefault) {
-        
-        AlphaInspection inspeccion = (AlphaInspection) eiInspeccionarCalidadFuenteCuarentena.getdata();
-        String fuenteURL = (String) inspeccion.getFuenteInfo().getData();
-        EvaluationValue eval = new EvaluationValue();
-        if(fuenteURL.equals("urn:fuente:1")){
-            eval.setObjectCriteria(0.8);
-            eval.setProcessCriteria(0.6);
-            eval.setSubjectCriteria(0.3);
-        }else if(fuenteURL.equals("urn:fuente:2")){
-            eval.setObjectCriteria(0.72);
-            eval.setProcessCriteria(0.7);
-            eval.setSubjectCriteria(0.18);
-        }else{
-            eval.setObjectCriteria(0.8);
-            eval.setProcessCriteria(0.59);
-            eval.setSubjectCriteria(0.6);
-        }
-        inspeccion.getFuenteInfo().setEval(eval);
-        outputsdefaultGradoCalidadFuenteCuarentena.setdata(inspeccion);
+	public static void processAlphaQualityInspectionRequestTaskTask(
+			InspectQualityOfSourceInTesting eiInspeccionarCalidadFuenteCuarentena,
+			QualityDegreeOfSourceInTesting outputsdefaultGradoCalidadFuenteCuarentena,
+			TaskOutput outputsdefault) {
 
-        System.out.println("Inspector: "+eval.getSubjectCriteria());
-    }
+		AlphaInspection inspeccion = (AlphaInspection) eiInspeccionarCalidadFuenteCuarentena.getdata();
+		String fuenteURL = (String) inspeccion.getFuenteInfo().getData();
+		EvaluationValue eval = new EvaluationValue();
+		if(fuenteURL.equals("urn:fuente:1")){
+			eval.setObjectCriteria(0.8);
+			eval.setProcessCriteria(0.6);
+			eval.setSubjectCriteria(0.3);
+		}else if(fuenteURL.equals("urn:fuente:2")){
+			eval.setObjectCriteria(0.72);
+			eval.setProcessCriteria(0.7);
+			eval.setSubjectCriteria(0.18);
+		}else{
+			eval.setObjectCriteria(0.8);
+			eval.setProcessCriteria(0.59);
+			eval.setSubjectCriteria(0.6);
+		}
+		inspeccion.getFuenteInfo().setEval(eval);
+		outputsdefaultGradoCalidadFuenteCuarentena.setprocessCriteria(eval.getProcessCriteria());
+		outputsdefaultGradoCalidadFuenteCuarentena.setsubjectCriteria(eval.getSubjectCriteria());
+		outputsdefaultGradoCalidadFuenteCuarentena.setobjectCriteria(eval.getObjectCriteria());
+		outputsdefaultGradoCalidadFuenteCuarentena.setdeclaredQuality(eiInspeccionarCalidadFuenteCuarentena.getdeclaredQuality());
+
+		outputsdefaultGradoCalidadFuenteCuarentena.setdata(inspeccion);
+
+		System.out.println("Inspector: "+eval.getSubjectCriteria());
+	}
+
+	public static void gaussianprocessAlphaQualityInspectionRequestTaskTask(
+			InspectQualityOfSourceInTesting eiInspeccionarCalidadFuenteCuarentena,
+			QualityDegreeOfSourceInTesting outputsdefaultGradoCalidadFuenteCuarentena,
+			TaskOutput outputsdefault) {
+
+		AlphaInspection inspeccion = (AlphaInspection) eiInspeccionarCalidadFuenteCuarentena.getdata();
+		String fuenteURL = (String) inspeccion.getFuenteInfo().getData();
+		EvaluationValue eval = new EvaluationValue();
+
+		eval.setObjectCriteria(Math.abs(randomSubject.nextGaussian()));
+		eval.setProcessCriteria(Math.abs(randomProcess.nextGaussian()));
+		eval.setSubjectCriteria(Math.abs(randomSubject.nextGaussian()));
+
+		inspeccion.getFuenteInfo().setEval(eval);
+		outputsdefaultGradoCalidadFuenteCuarentena.setprocessCriteria(eval.getProcessCriteria());
+		outputsdefaultGradoCalidadFuenteCuarentena.setsubjectCriteria(eval.getSubjectCriteria());
+		outputsdefaultGradoCalidadFuenteCuarentena.setobjectCriteria(eval.getObjectCriteria());
+		outputsdefaultGradoCalidadFuenteCuarentena.setdeclaredQuality(eiInspeccionarCalidadFuenteCuarentena.getdeclaredQuality());
+		outputsdefaultGradoCalidadFuenteCuarentena.setdata(inspeccion);
+
+		System.out.println("Inspector: "+eval.getSubjectCriteria());
+	}
+
+
+	public static void perfectprocessAlphaQualityInspectionRequestTaskTask(
+			InspectQualityOfSourceInTesting eiInspeccionarCalidadFuenteCuarentena,
+			QualityDegreeOfSourceInTesting outputsdefaultGradoCalidadFuenteCuarentena,
+			TaskOutput outputsdefault) {
+
+		AlphaInspection inspeccion = (AlphaInspection) eiInspeccionarCalidadFuenteCuarentena.getdata();
+		String fuenteURL = (String) inspeccion.getFuenteInfo().getData();
+		EvaluationValue eval = new EvaluationValue();
+
+		eval.setObjectCriteria(eiInspeccionarCalidadFuenteCuarentena.getdeclaredQuality());
+		eval.setProcessCriteria(eiInspeccionarCalidadFuenteCuarentena.getdeclaredQuality());
+		eval.setSubjectCriteria(eiInspeccionarCalidadFuenteCuarentena.getdeclaredQuality());
+
+		inspeccion.getFuenteInfo().setEval(eval);
+		outputsdefaultGradoCalidadFuenteCuarentena.setprocessCriteria(eval.getProcessCriteria());
+		outputsdefaultGradoCalidadFuenteCuarentena.setsubjectCriteria(eval.getSubjectCriteria());
+		outputsdefaultGradoCalidadFuenteCuarentena.setobjectCriteria(eval.getObjectCriteria());
+		outputsdefaultGradoCalidadFuenteCuarentena.setdeclaredQuality(eiInspeccionarCalidadFuenteCuarentena.getdeclaredQuality());
+
+		outputsdefaultGradoCalidadFuenteCuarentena.setdata(inspeccion);
+
+		System.out.println("Inspector: "+eval.getSubjectCriteria());
+	}
 
 }

d1d6df2c10c4720ec49cb5e61a81d4c04327f22b
escalope <jjgomez@fdi.ucm.es>
2013-03-26 22:07:20 +0000

    git-svn-id: svn://svn.code.sf.net/p/ingenias/code/trunk@509 95f194ad-9441-0410-9784-e312ea4ffdb5

--- IDKMaven/preparerelease.sh
+++ IDKMaven/preparerelease.sh
@@ -1,11 +1,11 @@
-env DISPLAY=:1 Xvfb :1 -screen 0 1280x1024x24 &
+env DISPLAY=:2 Xvfb :2 -screen 0 1280x1024x24 &
 sleep 2
-env DISPLAY=:1 fluxbox &
+env DISPLAY=:2 fluxbox &
 sleep 2
-env DISPLAY=:1 x11vnc -display :1 -bg -nopw -listen localhost -xkb&
+env DISPLAY=:2 x11vnc -display :2 -bg -nopw -listen localhost -xkb&
 sleep 2
 vncviewer  -viewonly  -encodings 'copyrect tight zrle hextile' localhost:5900 &
-env DISPLAY=:1 mvn release:$1
+env DISPLAY=:2 mvn release:$1
 killall -9 vncviewer
 killall -9 Xvfb
 
